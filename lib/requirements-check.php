37e9f1a1251b3d3eb5506714230f7e46830b22d7
githubsync <githubsync@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-28 13:04:54 +0000

    Update docs in requirements check class
    commit e0aed73abfdfed1515e51426bced3805664ff53a
    Author: Pascal Birchler <hello@pascalbirchler.ch>
    Date:   Tue Jul 28 15:04:33 2015 +0200
    
        Update docs in requirements check class
    
    commit 738c830f27b568ed30fb5b5744b12982e61e5f37
    Author: Pascal Birchler <hello@pascalbirchler.ch>
    Date:   Tue Jul 28 15:00:39 2015 +0200
    
        Show an admin notice when the REST API  isn't installed or older than 2.0-beta3. Fixes #23
    
    Synced from git://github.com/swissspidy/oEmbed-API.git
    
    
    git-svn-id: https://plugins.svn.wordpress.org/oembed-api/trunk@1208244 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/requirements-check.php
+++ lib/requirements-check.php
@@ -1,12 +1,47 @@
 <?php
+/**
+ * Simple requirements checking class.
+ *
+ * @package WP_API_oEmbed
+ */
 
 /**
  * Simple requirements checking class.
  */
 class WP_API_oEmbed_Requirements_Check {
+	/**
+	 * Default name of the plugin.
+	 *
+	 * @var string
+	 */
 	private $title = '';
+
+	/**
+	 * Default minimum required PHP version.
+	 *
+	 * @var string
+	 */
 	private $php = '5.2.4';
+
+	/**
+	 * Default minimum required WordPress version.
+	 *
+	 * @var string
+	 */
 	private $wp = '3.8';
+
+	/**
+	 * Default minimum required REST API version.
+	 *
+	 * @var string
+	 */
+	private $rest_api = '2.0';
+
+	/**
+	 * Path to the main plugin file.
+	 *
+	 * @var string
+	 */
 	private $file;
 
 	/**
@@ -15,7 +50,7 @@ class WP_API_oEmbed_Requirements_Check {
 	 * @param array $args An array of arguments to overwrite the default requirements.
 	 */
 	public function __construct( $args ) {
-		foreach ( array( 'title', 'php', 'wp', 'file' ) as $setting ) {
+		foreach ( array( 'title', 'php', 'wp', 'rest-api', 'file' ) as $setting ) {
 			if ( isset( $args[ $setting ] ) ) {
 				$this->$setting = $args[ $setting ];
 			}
@@ -23,10 +58,12 @@ class WP_API_oEmbed_Requirements_Check {
 	}
 
 	/**
+	 * Check if the install passes the requirements.
+	 *
 	 * @return bool True if the install passes the requirements, false otherwise.
 	 */
 	public function passes() {
-		$passes = $this->php_passes() && $this->wp_passes();
+		$passes = $this->php_passes() && $this->wp_passes() && $this->rest_api_passes();
 		if ( ! $passes ) {
 			add_action( 'admin_notices', array( $this, 'deactivate' ) );
 		}
@@ -44,10 +81,12 @@ class WP_API_oEmbed_Requirements_Check {
 	}
 
 	/**
+	 * Check if the PHP version passes the requirement.
+	 *
 	 * @return bool True if the PHP version is high enough, false otherwise.
 	 */
 	private function php_passes() {
-		if ( $this->__php_at_least( $this->php ) ) {
+		if ( $this->_php_at_least( $this->php ) ) {
 			return true;
 		} else {
 			add_action( 'admin_notices', array( $this, 'php_version_notice' ) );
@@ -58,8 +97,12 @@ class WP_API_oEmbed_Requirements_Check {
 
 	/**
 	 * Compare the current PHP version with the minimum required version.
+	 *
+	 * @param string $min_version The minimum required version.
+	 *
+	 * @return mixed
 	 */
-	private static function __php_at_least( $min_version ) {
+	private static function _php_at_least( $min_version ) {
 		return version_compare( phpversion(), $min_version, '>=' );
 	}
 
@@ -75,10 +118,12 @@ class WP_API_oEmbed_Requirements_Check {
 	}
 
 	/**
+	 * Check if the WordPress version passes the requirement.
+	 *
 	 * @return bool True if the WordPress version is high enough, false otherwise.
 	 */
 	private function wp_passes() {
-		if ( $this->__wp_at_least( $this->wp ) ) {
+		if ( $this->_wp_at_least( $this->wp ) ) {
 			return true;
 		} else {
 			add_action( 'admin_notices', array( $this, 'wp_version_notice' ) );
@@ -89,8 +134,12 @@ class WP_API_oEmbed_Requirements_Check {
 
 	/**
 	 * Compare the current WordPress version with the minimum required version.
+	 *
+	 * @param string $min_version The minimum required version.
+	 *
+	 * @return mixed
 	 */
-	private static function __wp_at_least( $min_version ) {
+	private static function _wp_at_least( $min_version ) {
 		return version_compare( get_bloginfo( 'version' ), $min_version, '>=' );
 	}
 
@@ -101,6 +150,47 @@ class WP_API_oEmbed_Requirements_Check {
 		?>
 		<div class="error">
 			<p><?php printf( 'The &#8220;%s&#8221; plugin cannot run on WordPress versions older than %s. Please update WordPress.', esc_html( $this->title ), $this->wp ); ?></p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Check if the REST API version passes the requirement.
+	 *
+	 * @return bool True if the REST API version is high enough, false otherwise.
+	 */
+	private function rest_api_passes() {
+		if ( $this->_rest_api_at_least( $this->rest_api ) ) {
+			return true;
+		} else {
+			add_action( 'admin_notices', array( $this, 'rest_api_version_notice' ) );
+
+			return false;
+		}
+	}
+
+	/**
+	 * Compare the current version of the REST API with the minimum required version.
+	 *
+	 * @param string $min_version The minimum required version.
+	 *
+	 * @return mixed
+	 */
+	private static function _rest_api_at_least( $min_version ) {
+		if ( ! defined( 'REST_API_VERSION' ) ) {
+			return false;
+		}
+
+		return version_compare( REST_API_VERSION, $min_version, '>=' );
+	}
+
+	/**
+	 * Show the REST API version notice.
+	 */
+	public function rest_api_version_notice() {
+		?>
+		<div class="error">
+			<p><?php printf( 'The &#8220;%s&#8221; plugin requires version %s or higher of the REST API to be installed. Please update or install the REST API plugin.', esc_html( $this->title ), $this->rest_api ); ?></p>
 		</div>
 		<?php
 	}
