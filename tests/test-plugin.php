d473cef81a7a63a975667d9410898938b27fc835
githubsync <githubsync@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-30 17:03:02 +0000

    Minor test fixes
    commit e37ea0624a3d046a0d61ad1baaed7f5c3d2f993a
    Author: Pascal Birchler <hello@pascalbirchler.ch>
    Date:   Thu Jul 30 19:02:40 2015 +0200
    
        Minor test fixes
    
    commit 65150db7b9172e3d70f2232b18b94407a432ff9b
    Author: Pascal Birchler <hello@pascalbirchler.ch>
    Date:   Thu Jul 30 19:02:26 2015 +0200
    
        Improve frontend class tests
    
    commit b0c5144379a9b5b18c3cdfd384e750b25817cdbe
    Author: Pascal Birchler <hello@pascalbirchler.ch>
    Date:   Thu Jul 30 18:25:55 2015 +0200
    
        Introduce new get_post_embed_url function
    
    Synced from git://github.com/swissspidy/oEmbed-API.git
    
    
    git-svn-id: https://plugins.svn.wordpress.org/oembed-api/trunk@1209834 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -38,9 +38,49 @@ class WP_API_oEmbed_Test_Plugin extends WP_API_oEmbed_TestCase {
 	 */
 	function test_add_oembed_provider() {
 		$oembed = _wp_oembed_get_object();
+
+		wp_oembed_remove_provider( home_url( '/*' ) );
+		$this->assertArrayNotHasKey( home_url( '/*' ), $oembed->providers );
+
+		$this->plugin()->add_oembed_provider();
+
 		$this->assertArrayHasKey( home_url( '/*' ), $oembed->providers );
 		$this->assertEquals( array( esc_url( rest_url( 'wp/v2/oembed' ) ), false ),
 			$oembed->providers[ home_url( '/*' ) ]
 		);
+	}
+
+	/**
+	 * Test the get_post_embed_url function.
+	 */
+	function test_get_post_embed_url_non_existent_post() {
+		$embed_url = get_post_embed_url( 0 );
+		$this->assertFalse( $embed_url );
+	}
+
+	/**
+	 * Test the get_post_embed_url function.
+	 */
+	function test_get_post_embed_url() {
+		update_option( 'permalink_structure', '/%postname%' );
+
+		$post_id   = $this->factory->post->create();
+		$permalink = get_permalink( $post_id );
+		$embed_url = get_post_embed_url( $post_id );
+
+		$this->assertEquals( $permalink . '/embed', $embed_url );
+
+		update_option( 'permalink_structure', '' );
+	}
+
+	/**
+	 * Test the get_post_embed_url function.
+	 */
+	function test_get_post_embed_url_pretty_permalinks() {
+		$post_id   = $this->factory->post->create();
+		$permalink = get_permalink( $post_id );
+		$embed_url = get_post_embed_url( $post_id );
+
+		$this->assertEquals( $permalink . '&embed=true', $embed_url );
 	}
 }
