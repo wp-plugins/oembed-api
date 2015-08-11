a152ac02e53eb105116632f75e009f71dacb4459
githubsync <githubsync@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-11 06:17:04 +0000

    And the rest of the version bump.
    commit 550e6ece221511421e246567f8f47156f2452116
    Author: Gary Pendergast <gary@pento.net>
    Date:   Tue Aug 11 16:16:51 2015 +1000
    
        And the rest of the version bump.
    
    Synced from git://github.com/swissspidy/oEmbed-API.git
    
    
    git-svn-id: https://plugins.svn.wordpress.org/oembed-api/trunk@1217883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-api-oembed.php
+++ wp-api-oembed.php
@@ -3,7 +3,7 @@
  * Plugin Name: oEmbed API
  * Plugin URI:  https://github.com/swissspidy/oEmbed-API
  * Description: An oEmbed provider for WordPress using the WP-API plugin.
- * Version:     0.2.0-20150811
+ * Version:     0.3.0-20150811
  * Author:      Pascal Birchler
  * Author URI:  https://spinpress.com
  * License:     GPLv2+
